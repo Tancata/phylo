e83f59c73cd70ea910c66c9a2ee0679b8c206bb8
Tom Williams <thwillia@gmail.com>
2016-09-15 15:40:58 +0100

    Improvements to tree subsampling

--- subsample_tree_by_branchlengths.py
+++ subsample_tree_by_branchlengths.py
@@ -1,41 +1,74 @@
 #!/usr/bin/env python
 #given a tree and a number of taxa n, print the n taxa that maximize the representation of branch lengths on the tree. This is supposed to help with a question like "I want a subsampled representation of this 3,000 taxon tree which retains as much of the phylogenetic diversity as possible but contains only n taxa"
-from Bio import Phylo
+from Bio import Phylo, AlignIO
 from collections import defaultdict
 import sys
 
-def tree_distances(t, tax): #calculate summed-branch length distances for all pairs of taxa on a tree
+def calc_all_distances(t): #given a tree, return all pairwise summed branch length distances
     distances = defaultdict(dict)
+    taxa = t.get_terminals()
+    for i in range(len(taxa)-1):
+        for j in range(i+1, len(taxa)):
+            ij_dist = t.distance(taxa[i],taxa[j])
+            distances[taxa[i].name][taxa[j].name] = ij_dist
+            distances[taxa[j].name][taxa[i].name] = ij_dist
+    return distances
+
+def tree_distances(taxa, dists): #calculate summed-branch length distances for all pairs of taxa on a tree
     smallest_dist = float(9999999)
     taxon_to_drop = ''
-    for i in range(len(tax)-1):
-        for j in range(len(tax)):
-            new_dist = t.distance(tax[i],tax[j])
+    for i in range(len(taxa)-1):
+        for j in range(i+1, len(taxa)):
+            new_dist = dists[taxa[i].name][taxa[j].name]
             if new_dist < smallest_dist:
-                taxon_to_drop = tax[i] #can setup a more complex selection here, e.g. if have genome size or completness, could drop the least complete
+                if len(sys.argv) <= 3:
+                    taxon_to_drop = taxa[i] #can setup a more complex selection here, e.g. if have genome size or completness, could drop the least complete
+                else:
+                    #drop taxon with greater number of gaps
+                    gaps_i = gaps[taxa[i].name]
+                    gaps_j = gaps[taxa[j].name]
+                    if gaps_i > gaps_j:
+                        taxon_to_drop = taxa[i]
+                    else:
+                        taxon_to_drop = taxa[j]
                 smallest_dist = new_dist
     return taxon_to_drop
 
+def count_gaps(aln):
+    gapc = {}
+    for record in aln:
+        gapc[record.id] = int(record.seq.count("-"))
+    return gapc 
+
 #tree - first arg
 #n - second arg
+#alignment (or something else with which to calculate completeness)
 
-if len(sys.argv) < 2:
-    print "Usage: subsample_tree_by_branchlengths.py NewickTreeFile NumToKeep"
+if len(sys.argv) < 3:
+    print "Usage: subsample_tree_by_branchlengths.py NewickTreeFile NumToKeep [optional: FastaAlignment]"
     quit()
+
 treefile = sys.argv[1]
 taxa_to_keep = int(sys.argv[2])
 
 tree = Phylo.read(sys.argv[1], "newick")
 taxa = tree.get_terminals()
 
+gaps = {}
+if len(sys.argv) > 3: #optional criterion to decide which taxon to drop, if supplied
+    alignment = AlignIO.read(sys.argv[3], "fasta")
+    gaps = count_gaps(alignment)
+
 #calculate all distances, then drop taxa with the smallest distance repeatedly until remaining number of taxa is n.
 
-
+all_distances = calc_all_distances(tree)
+	
 #now do recursive drop
 num_taxa = len(taxa)
 while num_taxa > taxa_to_keep:
     #get smallest distance, and drop one of the two taxa according to some other property, maybe
-    to_prune = tree_distances(tree, taxa)
+    print str(num_taxa)
+    to_prune = tree_distances(taxa, all_distances)
     tree.prune(to_prune)
     taxa = tree.get_terminals()
     num_taxa = len(taxa)
