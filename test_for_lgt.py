b40938eecea883f976c66936f4e975a5578c41d5
Tancata <thwillia@gmail.com>
2017-05-02 22:26:40 +0100

    Updated LGT checks a bit

--- test_for_lgt.py
+++ test_for_lgt.py
@@ -7,7 +7,8 @@ import sys, re
 
 
 tree = Tree(sys.argv[1])
-target_sequence_tag = sys.argv[2]
+#target_sequence_tag = sys.argv[2]
+target_sequence_tag = 'xxx'
 
 #setup a list of the eukaryotic sequences in the tree
 eukaryote_seqs = []
@@ -17,10 +18,18 @@ for leaf in tree:
         eukaryote_seqs.append(leaf.name)
     elif re.search(target_sequence_tag, leaf.name):
         eukaryote_seqs.append(leaf.name)
-
+#print eukaryote_seqs
 #test the various phylogenetic criteria for LGT.
 
 #euk sequence is a singleton nested within a clade of bacteria, and there is only one eukaryote sequence in the tree
+if len(eukaryote_seqs) == 1: #this is, I guess, an LGT candidate
+    print "LGT\tSingleton"
 #euk sequence is a singleton nested within a clade of bacteria, and the eukaryotes are not monophyletic in the tree
+print len(eukaryote_seqs)
+if tree.check_monophyly(values=eukaryote_seqs, target_attr="name"):
+    print "Euks monophyletic"
+else:
+    print "Euks not monophyletic" #this could, in itself, be a possible criterion for LGT --- or gene loss/transfer. Perhaps a condition that the target gene is not in the largest eukaryotic sub-clade (get_monophyletic() method will return subclades with a particular attribute that are monophyletic)
+#If euks are monophyletic, what is the max. number allowed for the gene to be considered a candidate LGT?
 #euk sequence is part of a euk clade nested within bacteria, and the eukaryotes are not monophyletic in the tree [what about the case where the LGT is the only copy in euks?]
 
