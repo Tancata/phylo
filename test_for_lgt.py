48f3a6e7c66ba1129e340b45be4ea60d3eb06de9
Tancata <thwillia@gmail.com>
2017-05-15 14:04:21 +0100

    Some updates to LGT testing script

--- test_for_lgt.py
+++ test_for_lgt.py
@@ -5,8 +5,19 @@ import sys, re
 
 #Arguments: treefile target_sequence_tag
 
+#check tree string for sanity first
+inh = open(sys.argv[1])
+treestring = inh.readline()
+treestr = treestring.replace(';','')
+treestr = treestr + ";"
+inh.close()
 
-tree = Tree(sys.argv[1])
+if len(treestr) == 0:
+    print sys.argv[1] + "\tEmpty tree"
+    quit()
+
+tree = Tree(treestr)
+out_tree = sys.argv[1] + ".pdf"
 #target_sequence_tag = sys.argv[2]
 target_sequence_tag = 'xxx'
 
@@ -23,13 +34,20 @@ for leaf in tree:
 
 #euk sequence is a singleton nested within a clade of bacteria, and there is only one eukaryote sequence in the tree
 if len(eukaryote_seqs) == 1: #this is, I guess, an LGT candidate
-    print "LGT\tSingleton"
+    print sys.argv[1] + "\tSingleton"
 #euk sequence is a singleton nested within a clade of bacteria, and the eukaryotes are not monophyletic in the tree
-print len(eukaryote_seqs)
-if tree.check_monophyly(values=eukaryote_seqs, target_attr="name"):
-    print "Euks monophyletic"
+#print len(eukaryote_seqs)
 else:
-    print "Euks not monophyletic" #this could, in itself, be a possible criterion for LGT --- or gene loss/transfer. Perhaps a condition that the target gene is not in the largest eukaryotic sub-clade (get_monophyletic() method will return subclades with a particular attribute that are monophyletic)
+    try:
+        if tree.check_monophyly(values=eukaryote_seqs, target_attr="name"):
+            ca = tree.get_common_ancestor(eukaryote_seqs)
+            print sys.argv[1] + "\tEuks monophyletic\t" + str(len(eukaryote_seqs)) + "\t" + str(ca.support) 
+        else:
+            print sys.argv[1] + "\tEuks not monophyletic" #this could, in itself, be a possible criterion for LGT --- or gene loss/transfer. Perhaps a condition that the target gene is not in the largest eukaryotic sub-clade (get_monophyletic() method will return subclades with a particular attribute that are monophyletic)
 #If euks are monophyletic, what is the max. number allowed for the gene to be considered a candidate LGT?
 #euk sequence is part of a euk clade nested within bacteria, and the eukaryotes are not monophyletic in the tree [what about the case where the LGT is the only copy in euks?]
-
+#tree.render(out_tree)
+    except:
+        print sys.argv[1] + "\t" + "\tEuks not monophyletic\t" + str(len(eukaryote_seqs))
+#uncomment the following to make a PDF of the tree
+#tree.render(out_tree)
