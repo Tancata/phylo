69833540c7ad8b5680c2cec24cf50184b97a4274
Tancata <thwillia@gmail.com>
2018-12-06 11:56:27 +0000

    Update branchwise script for origination values

--- ALE/branchwise_numbers_of_events.py
+++ ALE/branchwise_numbers_of_events.py
@@ -3,7 +3,6 @@ import os, sys, re
 from collections import defaultdict
 
 branchwise = defaultdict(list) #0th element: duplications, 1st element: transfers, second element: losses, third element: gene copy number
-
 input = [file for file in os.listdir(".") if file.endswith("uml_rec")]
 
 for file in input:
@@ -18,12 +17,15 @@ for file in input:
                 branchwise[line_elements[1]].append(float(line_elements[3]))
                 branchwise[line_elements[1]].append(float(line_elements[4]))
                 branchwise[line_elements[1]].append(float(line_elements[5]))
+                branchwise[line_elements[1]].append(float(line_elements[6]))
             else:
                 branchwise[line_elements[1]][0] += float(line_elements[2])
                 branchwise[line_elements[1]][1] += float(line_elements[3])
                 branchwise[line_elements[1]][2] += float(line_elements[4])
                 branchwise[line_elements[1]][3] += float(line_elements[5])
+                branchwise[line_elements[1]][4] += float(line_elements[6])
 
 #having looped over all reconciliation files, print out the total summary
+print "Branch\tDuplications\tTransfers\tLosses\tOriginations\tCopyNum"
 for entry in branchwise:
-    print entry + "\t" + str(branchwise[entry][0]) + "\t" + str(branchwise[entry][1]) + "\t" + str(branchwise[entry][2]) + "\t" + str(branchwise[entry][3])
+    print entry + "\t" + str(branchwise[entry][0]) + "\t" + str(branchwise[entry][1]) + "\t" + str(branchwise[entry][2]) + "\t" + str(branchwise[entry][3]) + "\t" + str(branchwise[entry][4])
