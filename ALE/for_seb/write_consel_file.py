996eefc8c777e99e88bcdfb7a51e992d8a0fa2f6
Tom Williams <tw15962@it032083.users.bris.ac.uk>
2020-10-02 10:56:41 +0100

    Some updates

---
+++ ALE/for_seb/write_consel_file.py
@@ -0,0 +1,51 @@
+#Read a directory of gene tree reconciliation files produced by ALEml_undated, pull out the gene family likelihoods, and write a (line?) of a CONSEL mt file.
+#As written, the CONSEL input file will be written just for that subset of families for which a .uml_rec file was produced under each rooted tree. No information is printed about the family/root combinations for which uml_rec output files could not be found.
+
+import os, re, sys
+dirs = sys.argv[1:]
+trees = {}
+summed_lnls = {}
+
+if len(dirs) < 2:
+    print("Usage: write_consel_file.py <space-delimited list of directories containing identically named reconciliation files to compare>")
+    quit()
+
+def extract_logl(rec_file):
+    inh = open(rec_file)
+    for line in inh:
+        if line.startswith(">logl"):
+            fields = re.split("\s+", line.rstrip())
+            likelihood = fields[1]
+    inh.close()
+    return likelihood
+
+def check_have_reconciliations(filename, dir_list): #given a reconciliation file (e.g. in the first directory, check whether the same gene family has also been reconciled in each of the other directories
+    have_them = 1
+    for dir in dir_list:
+        if os.path.exists(dir + "/" + filename):
+            continue
+        else:
+            have_them = 0
+    return have_them
+
+#identify the pool of gene fams for which reconciliation under all candidate rooted trees worked, get and print their lnls
+score_num = 0
+to_do = [file for file in os.listdir(dirs[0] + "/") if file.endswith("uml_rec")]
+for file in to_do:
+    have_them = check_have_reconciliations(file, dirs) 
+    if have_them == 1:
+        for dir in dirs:
+            tag = "#" + dir
+            lnl = extract_logl(dir + "/" + file)
+            if tag in trees:
+                trees[tag] = trees[tag] + str(lnl) + " "
+                summed_lnls[tag] = summed_lnls[tag] + float(lnl)
+            else:
+                trees[tag] = str(lnl) + " "
+                summed_lnls[tag] = float(lnl)
+        score_num += 1
+scores = ''
+print(str(len(dirs)) + " " + str(score_num))
+for dir in dirs:
+    tag = "#" + dir
+    print(trees[tag] + "_" + str(summed_lnls[tag]) + "\n" + trees[tag])
